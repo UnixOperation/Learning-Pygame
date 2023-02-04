dd8dc4e5b67d7ce41a331592ad5486e93ec8a05d
[Information Redacted by Gliath 729] <123034777+UnixOperation@users.noreply.github.com>
2023-02-04 16:42:16 +0000

    Create pygame_file_01.py

---
+++ Tutorial/pygame_file_01.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+width = 500
+height = 500
+screen = pygame.display.set_mode((width, height))
+
+running = False
+while running:
+  for event.type == pygame.QUIT:
+    running = False
+    
+  pygame.display.update()
